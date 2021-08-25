4bf6e5b756143d6fb88b47075cf772468cde8566
Eugene Ivchenko <goldenjoe@outlook.com>
2021-08-25 19:46:50 +0300

    [runtime-checks] Support DisallowNull, add more tests

--- src/Compilers/CSharp/Test/Emit/RuntimeChecks/RuntimeNullCheckTests_JetBrains.cs
+++ src/Compilers/CSharp/Test/Emit/RuntimeChecks/RuntimeNullCheckTests_JetBrains.cs
@@ -158,6 +158,36 @@ class C : I
             var comp = CreateCompilation(source, nullableContext: false);
             CompileAndVerifyException<ArgumentNullException>(comp);
         }
+        
+        [Fact]
+        [Trait("Annotations", "JetBrains")]
+        public void NotNull_InheritedFromInterface_ImplementedInBaseType()
+        {
+            const string source = @"
+using System;
+using JetBrains.Annotations;
+interface I
+{
+    void Goo([NotNull] string s);
+}
+
+class B : I
+{
+    public void Goo(string s) { }
+}
+
+class C : B
+{
+    static void Main()
+    {
+        var c = new C();
+        c.Goo(null);
+    }
+}";
+
+            var comp = CreateCompilation(source, nullableContext: false);
+            CompileAndVerifyException<ArgumentNullException>(comp);
+        }
 
         [Fact]
         [Trait("Annotations", "JetBrains")]
@@ -183,6 +213,35 @@ class C : I3
     }
 
     public void Goo(string s)
+    {
+        Console.WriteLine(s.Length);
+    }
+}";
+
+            var comp = CreateCompilation(source, nullableContext: false);
+            CompileAndVerifyException<ArgumentNullException>(comp);
+        }
+        
+        [Fact]
+        [Trait("Annotations", "JetBrains")]
+        public void NotNull_InheritedFromInterface_ExplicitImplementation()
+        {
+            const string source = @"
+using System;
+using JetBrains.Annotations;
+interface I
+{
+    void Goo([NotNull] string s);
+}
+class C : I
+{
+    static void Main()
+    {
+        var c = new C();
+        ((I)c).Goo(null);
+    }
+
+    void I.Goo(string s)
     {
         Console.WriteLine(s.Length);
     }
@@ -317,6 +376,65 @@ class D : I
 
             var comp = CreateCompilation(source, nullableContext: false);
             CompileAndVerifyException<ArgumentNullException>(comp);
+        }
+        
+        [Fact]
+        [Trait("Annotations", "JetBrains")]
+        public void NotNull_GenericAutoProperty_WithSetter()
+        {
+            const string source = @"
+using System;
+using JetBrains.Annotations;
+class Generic<T>
+{
+    [NotNull] public T Prop { get; set; }
+}
+
+class C
+{
+    static void Main()
+    {
+        var g = new Generic<string>();
+        g.Prop = null;
+    }
+}";
+
+            var comp = CreateCompilation(source, nullableContext: false);
+            CompileAndVerifyException<ArgumentNullException>(comp);
+        }
+
+        [Fact]
+        [Trait("Annotations", "JetBrains")]
+        public void NotNull_GenericAutoProperty_StructConstraint_NoCheck()
+        {
+            const string source = @"
+using System;
+using JetBrains.Annotations;
+class Generic<T> where T : struct
+{
+    [NotNull] public T Prop { get; set; }
+}
+
+class C
+{
+    static void Main()
+    {
+        var g = new Generic<int>();
+        g.Prop = 42;
+    }
+}";
+
+            var comp = CreateCompilation(source, nullableContext: false);
+            var verifier = CompileAndVerify(comp);
+            verifier.VerifyIL("Generic<T>.Prop.set", @"
+{
+  // Code size        8 (0x8)
+  .maxstack  2
+  IL_0000:  ldarg.0
+  IL_0001:  ldarg.1
+  IL_0002:  stfld      ""T Generic<T>.<Prop>k__BackingField""
+  IL_0007:  ret
+}");
         }
 
         [Fact]
