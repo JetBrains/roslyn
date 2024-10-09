10304a76df891bcc6c26dfb90be6dbba8b70b585
Aleksandr Kugushev <alexandr.kugushev@jetbrains.com>
2024-10-09 15:55:19 +0200

    Jet Patch: v4.12.0to243_5ef52ae_SDK9rc2
    New hacks to support the new Roslyn version:
    - They added $NET compilation variable, so we added $NETCOREAPP3_1 to fix compile errors
    - Fix compilation error in EditSession.cs with ArrayBuilder dummy
    - We have a hack in CSharpSyntaxGenerator.csproj (CompileInclude SyntaxKind.cs..). And this hack lead to problems ... fixed with the other hack in SyntaxKind.cs

--- src/Dependencies/Contracts/NullableAttributes.cs
+++ src/Dependencies/Contracts/NullableAttributes.cs
@@ -90,7 +90,7 @@ namespace System.Diagnostics.CodeAnalysis
 
 #endif
 
-#if !NETCOREAPP
+#if !NETCOREAPP || NETCOREAPP3_1
 
     /// <summary>Specifies that the method or property will ensure that the listed field and property members have not-null values.</summary>
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property, Inherited = false, AllowMultiple = true)]
